53977d06c7d7ce16f93c26b0a98084dcefabf957
wehong <we_hong@hanmail.net>
2025-03-26 00:16:01 +0900

    Update pcremake.py

--- pcremake.py
+++ pcremake.py
@@ -7,7 +7,6 @@ screen_height = 480
 pygame.display.set_caption("Putty Camiyon Remake")
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-#dt = 0
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, position):
@@ -43,19 +42,16 @@ class Player(pygame.sprite.Sprite):
             self.x = 640
     def bounce_up(self):
         self.vy = -130
-        #print("up")
     def bounce_left(self):
         self.vy = -100
         self.x -= 20
         if self.x <= 0:
             self.x = 0
-        #print("left")
     def bounce_right(self):
         self.vy = -100
         self.x += 20
         if self.x >= 640:
             self.x = 640
-        #print("right")
     def bounce_down(self):
         self.vy -= 2
         self.y += 3
@@ -88,7 +84,6 @@ class Balloon(pygame.sprite.Sprite):
         self.rect.center = (self.x, self.y)#self.position
 
 def main():
-    #player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
     running = True
     g_over = True
@@ -101,7 +96,6 @@ def main():
         Balloon((600, 280)),
         Balloon((600, 400))
     ]
-    #bubble_sprites = pygame.sprite.Group(bubbles)
     balloon_group = pygame.sprite.RenderPlain(*balloons)
     
     bounce_sound = pygame.mixer.Sound("bounce.ogg" )
@@ -110,31 +104,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        #screen.fill("black")
-        #pygame.draw.circle(screen, "red", player_pos, 40)
         
         keys = pygame.key.get_pressed()
-        #if keys[pygame.K_UP]:
-        #    player_pos.y -= 10#300 * dt
-        #    if player_pos.y <= 0:
-        #        player_pos.y = 0
-        #if keys[pygame.K_DOWN]:
-        #    player_pos.y += 10#300 * dt
-        #    if player_pos.y >= screen_height:
-        #        player_pos.y = screen_height
+        
         if keys[pygame.K_LEFT]:
-            player.move_lef()#rect.move_ip(-4, 0)
-        #    player_pos.x -= 10#300 * dt
-        #    if player_pos.x <=0:
-        #        player_pos.x = 0
+            player.move_lef()
         if keys[pygame.K_RIGHT]:
-            player.move_right()#.rect.move_ip(4, 0)
-        #    player_pos.x += 10#300 * dt
-        #    if player_pos.x >= screen_width:
-        #        player_pos.x = screen_width
+            player.move_right()
         if player.check_crash() == True:
-            #print("Crashed!")
             running = False
 
         collisions = pygame.sprite.spritecollide(player, balloon_group, False)
@@ -156,18 +133,15 @@ def main():
             collisions = None
 
         player_sprite.update()
-        #bubble_sprites.update()
         balloon_group.update()
 
         screen.fill("black")
         pygame.draw.line(screen, (0,255, 0), [0, 475], [640,475], 10)
         player_sprite.draw(screen)
-        #bubble_sprites.draw(screen)
         balloon_group.draw(screen)
 
         pygame.display.flip()
         
-        #dt = clock.tick(60) / 1000
         clock.tick(60)
 
 if __name__ == '__main__':
